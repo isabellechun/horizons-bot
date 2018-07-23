efe39ce9952453949944a3dfcb338744ca6f18d9
rtang4 <rtang4@binghamton.edu>
2018-07-23 16:21:16 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete credentials.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'token.json';
 
+// var client_id = "451652454399-qv2nhnkllt4mfqnt8m14r2b9bpp2hbko.apps.googleusercontent.com"
+// var client_secret="UwmVcm9dYQE1AyBg2Nx3lzQM"
+// var redirect_uris= ["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+// authorize({client_id,client_secret,redirect_uris},createEvent)
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), createEvent);
 });
 
 /**
@@ -67,6 +71,7 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
+ /*
 function listEvents(auth) {
   const calendar = google.calendar({version: 'v3', auth});
   calendar.events.list({
@@ -88,4 +93,47 @@ function listEvents(auth) {
       console.log('No upcoming events found.');
     }
   });
+} */
+function createEvent(auth){
+  var event = {
+    'summary': 'Roberts midnight dream',
+    'location': 'hell hole',
+    'description': 'A chance to hear more about Google\'s developer products.',
+    'start': {
+      'dateTime': '2018-07-23T09:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': '2018-07-23T17:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    /*'recurrence': [
+      'RRULE:FREQ=DAILY;COUNT=2'
+    ],
+    'attendees': [
+      {'email': 'lpage@example.com'},
+      {'email': 'sbrin@example.com'},
+    ],
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    }, */
+  };
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.insert({
+    auth: auth,
+    calendarId: 'primary',
+    resource: event,
+  }, function(err, event) {
+    if (err) {
+      console.log('There was an error contacting the Calendar service: ' + err);
+      return;
+    }
+    console.log(event)
+    console.log('Event created: %s', event.data.htmlLink);
+  });
+
 }
