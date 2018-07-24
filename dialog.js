1bd060d94d370b307782e3f26b7a4daace6e6692
Dhruva Mehta <30417337+dhruva-mehta@users.noreply.github.com>
2018-07-24 11:43:58 -0700

    mongoose models

--- dialog.js
+++ dialog.js
@@ -5,6 +5,9 @@ import googleCal from './index'
 // Instantiate a DialogFlow client
 const dialogflow = require('dialogflow');
 const sessionClient = new dialogflow.SessionsClient();
+const User = require('./models').User;
+const Task = require('./models').Task;
+
 
 // Define session path
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
@@ -33,7 +36,7 @@ export default function dflow(query, callback) {
         //parse result.queryText
         console.log('Detected intent in dialog.js');
         console.log("Text is: " + result.fulfillmentText)
-        var resp = result.fulfillmentText.split(',');
+        var resp = result.fulfillmentText.split(',;,');
         //resp[0] = task, resp[1] = subject, resp[2] = date-time
         var obj = {
           intent: result.intent.displayName,
@@ -41,7 +44,16 @@ export default function dflow(query, callback) {
           subject: resp[1],
           date: resp[2]
         };
-        callback(obj);
+        var task = new Task({
+          task: resp[0],
+          subject: resp[1],
+          date: resp[2]
+        });
+        task.save(function(err, success) {
+          if (err) console.log('err in saving', err)
+          else () => callback(obj)
+        })
+
         //console.log("Dialog: " + obj)
 
         //googleCal(obj)
